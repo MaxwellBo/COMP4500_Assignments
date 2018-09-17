e8ebbfecaf900eb48309a8bb5fe9917e5bde77d1
Max Bo <max@maxbo.me>
2018-09-17 12:14:01 +1000

    Update

--- Assignment_1/report/graph.py
+++ Assignment_1/report/graph.py
@@ -1,5 +1,6 @@
 import networkx as nx
 from collections import namedtuple
+import os
 
 G = nx.DiGraph()
 
@@ -14,6 +15,19 @@ class Node(object):
         self.d = None
         self.f = None
 
+    def gen_label(self):
+
+        t = ""
+
+        if self.d:
+            t += str(self.d) + "/"
+
+        if self.f:
+            t += str(self.f)
+
+        return "{ " +str(self.uid) + " | " + str(t) + " }"
+
+
     def __hash__(self):
         return hash(self.uid)
 
@@ -21,17 +35,33 @@ class Node(object):
 for (u, v) in zip(sni[:-1], sni[1:]):
     G.add_edge(Node(u), Node(v))
 
-def dump_graph(G):
-    print("digraph G {")
-    for (u, v) in G.edges:
-        print("{h} [fillcolor={hf}] -> {t} [fillcolor={tf}]".format(
-            h=u.uid,
-            hf=u.color.lower(),
-            t=v.uid,
-            tf=v.color.lower()
-        ))
-    print("0")
-    print("}")
+
+
+def dump_graph(G, t):
+    with open(f"{t}.graph", "w") as f:
+
+        f.write("digraph G {\n")
+
+        for u in G.nodes:
+            # print("{} [fillcolor=\"{}\", style=\"filled\"]".format(u.uid, u.color.lower()))
+            f.write("{} [fillcolor=\"{}\", style=\"filled\", shape=\"record\", label=\"{}\"]\n".format(
+                u.uid,
+                "yellow",
+                u.gen_label()
+                ))
+
+        for (u, v) in G.edges:
+            f.write("{h} -> {t}\n".format(
+                h=u.uid,
+                t=v.uid
+            ))
+        f.write("0\n")
+        f.write("}\n")
+
+
+    os.system(f"dot -Tpng {t}.graph > {t}.png")
+
+# dump_graph(G)
 
 time = 0
 
@@ -43,6 +73,7 @@ def dfs(G):
 def dfs_visit(G, u):
     global time
     time = time + 1
+    dump_graph(G, time)
     u.d = time
     u.color = "GRAY"
 
@@ -56,8 +87,5 @@ def dfs_visit(G, u):
     u.f = time
 
 dfs(G)
-
-dump_graph(G)
-
 
 # nx.draw_networkx(G)
